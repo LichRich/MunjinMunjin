fb76824bce6cfc102df010492d82c11eb081e092
LichRich <leech135789@gmail.com>
2020-09-24 10:47:59 +0900

    webView Example complete 1. commit except google-services.json 2. this is just example for MunjinMunjin.

--- app/src/main/java/com/example/android/munjinex/MainActivity.java
+++ app/src/main/java/com/example/android/munjinex/MainActivity.java
@@ -1,46 +1,171 @@
 package com.example.android.munjinex;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
 
-    WebView web;
+    WebView webView;
+    TextView errorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        LinearLayout layout = new LinearLayout(this);
-        layout.setOrientation(LinearLayout.VERTICAL);
-        web = new WebView(this);
-        WebSettings webSettings = web.getSettings();
+        FirebaseMessaging.getInstance().subscribeToTopic("notice");
+
+        errorView = (TextView) findViewById(R.id.tv_error);
+        webView = (WebView) findViewById(R.id.wv_main);
+
+        WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        webSettings.setUseWideViewPort(true);
-        webSettings.setBuiltInZoomControls(false);
-        webSettings.setAllowUniversalAccessFromFileURLs(true);
-        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-        webSettings.setSupportMultipleWindows(true);
-        webSettings.setSaveFormData(false);
-        webSettings.setSavePassword(false);
-        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
-        web.setWebChromeClient(new WebChromeClient());
-        web.setWebViewClient(new WebViewClient());
-        web.loadUrl("file:///android_asset/MunjinMunjin.html");
-        layout.addView(web);
 
-        setContentView(layout);
+        webView.setWebViewClient(new WebViewClient() {
+            @SuppressWarnings("deprecation")
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+
+            @TargetApi(Build.VERSION_CODES.N)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                switch (error.getErrorCode()) {
+                    case ERROR_AUTHENTICATION:
+                    case ERROR_BAD_URL:
+                    case ERROR_CONNECT:
+                    case ERROR_FAILED_SSL_HANDSHAKE:
+                    case ERROR_FILE:
+                    case ERROR_FILE_NOT_FOUND:
+                    case ERROR_HOST_LOOKUP:
+                    case ERROR_IO:
+                    case ERROR_PROXY_AUTHENTICATION:
+                    case ERROR_REDIRECT_LOOP:
+                    case ERROR_TIMEOUT:
+                    case ERROR_TOO_MANY_REQUESTS:
+                    case ERROR_UNKNOWN:
+                    case ERROR_UNSUPPORTED_AUTH_SCHEME:
+                    case ERROR_UNSUPPORTED_SCHEME: break;
+                }
+                super.onReceivedError(view, request, error);
+                webView.setVisibility(View.GONE);
+                errorView.setVisibility(View.VISIBLE);
+            }
+
+            @TargetApi(Build.VERSION_CODES.M)
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                switch (errorCode) {
+                    case ERROR_AUTHENTICATION:
+                    case ERROR_BAD_URL:
+                    case ERROR_CONNECT:
+                    case ERROR_FAILED_SSL_HANDSHAKE:
+                    case ERROR_FILE:
+                    case ERROR_FILE_NOT_FOUND:
+                    case ERROR_HOST_LOOKUP:
+                    case ERROR_IO:
+                    case ERROR_PROXY_AUTHENTICATION:
+                    case ERROR_REDIRECT_LOOP:
+                    case ERROR_TIMEOUT:
+                    case ERROR_TOO_MANY_REQUESTS:
+                    case ERROR_UNKNOWN:
+                    case ERROR_UNSUPPORTED_AUTH_SCHEME:
+                    case ERROR_UNSUPPORTED_SCHEME: break;
+                }
+                super.onReceivedError(view, errorCode, description, failingUrl);
+                webView.setVisibility(View.GONE);
+                errorView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+
+//            alert
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, final JsResult result) {
+                new AlertDialog.Builder(view.getContext())
+                        .setTitle("알림")
+                        .setMessage(message)
+                        .setPositiveButton(android.R.string.ok, new AlertDialog.OnClickListener(){
+
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                result.confirm();
+                            }
+                        })
+                        .setCancelable(false)
+                        .create()
+                        .show();
+                return true;
+            }
+
+//            confirm
+
+            @Override
+            public boolean onJsConfirm(WebView view, String url, String message, final JsResult result) {
+                new AlertDialog.Builder(view.getContext())
+                        .setTitle("알림")
+                        .setMessage(message)
+                        .setPositiveButton("Yes", new AlertDialog.OnClickListener(){
+
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                result.confirm();
+                            }
+                        })
+                        .setNegativeButton("No", new AlertDialog.OnClickListener() {
+
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                result.cancel();
+                            }
+                        })
+                        .setCancelable(false)
+                        .create()
+                        .show();
+                return true;
+            }
+        });
+
+        webView.loadUrl("https://m.naver.com");
     }
 
-    public void onBackPressed(){
-        if (web.canGoBack()) web.goBack();
-        else finish();
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK){
+            if (webView.canGoBack()){
+                webView.goBack();
+                return false;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
---
+++ app/src/main/java/com/example/android/munjinex/MyFirebaseMessagingService.java
@@ -0,0 +1,55 @@
+package com.example.android.munjinex;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "FMC log";
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        Log.d(TAG, "Refreshed token: " + s);
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        if (remoteMessage.getNotification() != null){
+            Log.d(TAG, "알림: " + remoteMessage.getNotification().getBody());
+            String messageBody = remoteMessage.getNotification().getBody();
+            String messageTitle = remoteMessage.getNotification().getTitle();
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+            String channelId = "Channel ID";
+            Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle(messageTitle)
+                    .setContentText(messageBody)
+                    .setAutoCancel(true)
+                    .setSound(defaultSoundUri)
+                    .setContentIntent(pendingIntent);
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                String channelName = "Channel Name";
+                NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+                notificationManager.createNotificationChannel(channel);
+            }
+            notificationManager.notify(0, notificationBuilder.build());
+        }
+    }
+}
