e9ab6ddaa68d25cb36dfc4d1f60bb242d015b42a
LichRich <leech135789@gmail.com>
2020-10-08 15:01:35 +0900

    이비인후과 문진표 수정 1. HTML 정리 2. js로 text 활성화

--- app/src/main/assets/js/Munjinpyo_script.js
+++ app/src/main/assets/js/Munjinpyo_script.js
@@ -15,13 +15,17 @@ for (let i = 0 ; i < boneRbHurts.length; i++){
   }
 }
 
-const cbPPO = document.getElementById("cbPainPointOther");
-cbPPO.onchange = function() {
-  if (cbPPO.checked) {
-    document.getElementById("txtPainPointOther").disabled = false;
-  } else {
-    document.getElementById("txtPainPointOther").disabled = true;
+try {
+  const cbPPO = document.getElementById("cbPainPointOther");
+  cbPPO.onchange = function() {
+    if (cbPPO.checked) {
+      document.getElementById("txtPainPointOther").disabled = false;
+    } else {
+      document.getElementById("txtPainPointOther").disabled = true;
+    }
   }
+} catch (e) {
+
 }
 
 try {
@@ -62,6 +66,60 @@ for (let i = 0 ; i < rbAllergy.length; i++){
 /************************* DENTIST ************************/
 
 /************************* EAR ************************/
+
+try {
+  const cbDHO = document.getElementById("cb_dhOther");
+  cbDHO.onchange = function () {
+    if (cbDHO.checked) {
+      document.getElementById("txt_dhOther").disabled = false;
+      document.getElementById("txt_dhOther").placeholder = "병명을 입력하세요.";
+    } else {
+      document.getElementById("txt_dhOther").disabled = true;
+      document.getElementById("txt_dhOther").placeholder = "";
+    }
+  }
+} catch (e) {
+
+}
+
+const rbDrugSide = document.getElementsByName("rb_drugSide");
+for (let i = 0 ; i < rbDrugSide.length; i++){
+  rbDrugSide[i].onclick = function () {
+    if (rbDrugSide[0].checked) {
+      document.getElementById("txt_drugSide").disabled = false;
+      document.getElementById("txt_drugSide").placeholder = "약물 이름을 적어주세요";
+    } else if(rbDrugSide[1].checked) {
+      document.getElementById("txt_drugSide").disabled = true;
+      document.getElementById("txt_drugSide").placeholder = "";
+    }
+  }
+}
+
+const rbOtherAction = document.getElementsByName("rb_otherAction");
+for (let i = 0 ; i < rbOtherAction.length; i++){
+  rbOtherAction[i].onclick = function () {
+    if (rbOtherAction[0].checked) {
+      document.getElementById("txt_OAAHos").disabled = false;
+      document.getElementById("txt_OAAAct").disabled = false;
+    } else if(rbOtherAction[1].checked) {
+      document.getElementById("txt_OAAHos").disabled = true;
+      document.getElementById("txt_OAAAct").disabled = true;
+    }
+  }
+}
+
+const rbVisited = document.getElementsByName("rb_visited");
+for (let i = 0 ; i < rbVisited.length; i++){
+  rbVisited[i].onclick = function () {
+    if (rbVisited[0].checked) {
+      document.getElementById("ta_symtomChange").disabled = false;
+      document.getElementById("ta_symtomChange").placeholder = "증세 변화 사항 및 필요한 사항을 적어주세요.";
+    } else if(rbVisited[1].checked) {
+      document.getElementById("ta_symtomChange").disabled = true;
+      document.getElementById("ta_symtomChange").placeholder = "";
+    }
+  }
+}
 
 /************************* EYE ************************/
 
